7c1a3692a733f91c6839245df690a90f08d78347
Koirala <mrkoirala58@gmail.com>
2021-02-24 11:22:14 +0545

    Initial commit

---
+++ LABexercise1/Qno.1.py
@@ -0,0 +1,6 @@
+#WAP that takes three num and print their sum. Every num is given on seperate line.
+
+num_one =float(input("Enter first num:" ))
+num_two =float(input("Enter second num:" ))
+num_three =float(input("Enter third num: "))
+print(num_one+num_two+num_three)

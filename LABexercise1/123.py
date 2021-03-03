db8ca171a153a80dd3fd7c2034ec1825ebbfc79c
Koirala <mrkoirala58@gmail.com>
2021-03-03 11:36:15 +0545

    initial commit

---
+++ LABexercise1/123.py
@@ -0,0 +1,9 @@
+#WAP to find a number whether it is +ve -ve or 0
+
+num= int(input("Enter a num;"))
+if num>0:
+    print("The num is positive")
+elif num<0:
+    print("The num is negative")
+else:
+    print("The num is zero")
